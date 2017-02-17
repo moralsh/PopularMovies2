51c56432c83e43924476fbd0bd486957bf5bdd8c
morals <morals@ctu_109_32.d-ctu.uned.es>
2017-02-17 14:39:57 +0100

    Added subclasses reviews and videos to movie class

--- app/src/main/java/org/moralsh/android/popularmovies/Movie.java
+++ app/src/main/java/org/moralsh/android/popularmovies/Movie.java
@@ -85,11 +85,143 @@ public class Movie {
     }
 
     public class Reviews {
+        private String id;
+        private String author;
+        private String content;
+        private String url;
 
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public  void jsonParse(JSONObject json) {
+            try {
+                setId(json.getString("id"));
+                setAuthor(json.getString("author"));
+                setContent(json.getString("content"));
+                setUrl(json.getString("url"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public class Videos {
+        private String id;
+        private String iso_639_1;
+        private String iso_3166_1;
+        private String key;
+        private String name;
+        private String site;
+        private int size;
+        private String type;
 
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getIso_639_1() {
+            return iso_639_1;
+        }
+
+        public void setIso_639_1(String iso_639_1) {
+            this.iso_639_1 = iso_639_1;
+        }
+
+        public String getIso_3166_1() {
+            return iso_3166_1;
+        }
+
+        public void setIso_3166_1(String iso_3166_1) {
+            this.iso_3166_1 = iso_3166_1;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public void setKey(String key) {
+            this.key = key;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getSite() {
+            return site;
+        }
+
+        public void setSite(String site) {
+            this.site = site;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public void setSize(int size) {
+            this.size = size;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public  void jsonParse(JSONObject json) {
+            try {
+                setId(json.getString("id"));
+                setIso_639_1(json.getString("iso_639_1"));
+                setIso_3166_1(json.getString("iso_4166"));
+                setKey(json.getString("key"));
+                setName(json.getString("name"));
+                setSite(json.getString("site"));
+                setSize(json.getInt("size"));
+                setType(json.getString("type"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 
